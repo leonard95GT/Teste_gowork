2465adad2a15a6fbb309510a3b962e0ac4a47726
Leonardo Rene <leonardorenepereira@gmail.com>
2020-05-13 15:46:36 -0300

    autenticações nas rotas

--- backend_node/start/routes.js
+++ backend_node/start/routes.js
@@ -21,25 +21,25 @@ Route.get('/', () => {
 })
 
 //office's
-Route.get ('/office', 'OfficeController.index')
-Route.post('/office', 'OfficeController.store')
-Route.post('/office/:id', 'OfficeController.search')
-Route.patch('/office/:id', 'OfficeController.update')
-Route.delete('/office/:id', 'OfficeController.destroy')
+Route.get ('/office', 'OfficeController.index').middleware(['auth'])
+Route.post('/office', 'OfficeController.store').middleware(['auth'])
+Route.post('/office/:id', 'OfficeController.search').middleware(['auth'])
+Route.patch('/office/:id', 'OfficeController.update').middleware(['auth'])
+Route.delete('/office/:id', 'OfficeController.destroy').middleware(['auth'])
 
 //Coworking plan's
-Route.get ('/coworking-plan', 'CoworkingPlanController.index')
-Route.post('/coworking-plan', 'CoworkingPlanController.store')
-Route.post('/coworking-plan/:id', 'CoworkingPlanController.search')
-Route.patch('/coworking-plan/:id', 'CoworkingPlanController.update')
-Route.delete('/coworking-plan/:id', 'CoworkingPlanController.destroy')
+Route.get ('/coworking-plan', 'CoworkingPlanController.index').middleware(['auth'])
+Route.post('/coworking-plan', 'CoworkingPlanController.store').middleware(['auth'])
+Route.post('/coworking-plan/:id', 'CoworkingPlanController.search').middleware(['auth'])
+Route.patch('/coworking-plan/:id', 'CoworkingPlanController.update').middleware(['auth'])
+Route.delete('/coworking-plan/:id', 'CoworkingPlanController.destroy').middleware(['auth'])
 
 //office's
-Route.get ('/client', 'ClientController.index')
-Route.post('/client', 'ClientController.store')
-Route.post('/client/:id', 'ClientController.search')
-Route.patch('/client/:id', 'ClientController.update')
-Route.delete('/client/:id', 'ClientController.destroy')
+Route.get ('/client', 'ClientController.index').middleware(['auth'])
+Route.post('/client', 'ClientController.store').middleware(['auth'])
+Route.post('/client/:id', 'ClientController.search').middleware(['auth'])
+Route.patch('/client/:id', 'ClientController.update').middleware(['auth'])
+Route.delete('/client/:id', 'ClientController.destroy').middleware(['auth'])
 
 //user's
 Route.post('/register', 'UserController.create')
